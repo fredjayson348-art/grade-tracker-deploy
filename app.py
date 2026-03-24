45cc59d6823229c531c29225051ffe7dec986808
fredjayson348-art <fredjayson348@gmail.com>
2026-03-24 22:06:13 +0000

    fix init_db position

--- app.py
+++ app.py
@@ -14,6 +14,8 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB = os.path.join(BASE_DIR, 'grades.db')
 init_db()
 
+init_db()
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
 def get_db():
